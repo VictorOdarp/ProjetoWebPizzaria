bcd136756eb2e6caf68c2ba57263c46fce71ef8c
Victor Eduardo <VictorOdarp@gmail.com>
2023-12-28 17:32:42 -0300

    Primeiro commit - Teste UsersController e Users View

---
+++ DarpinosPizza/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DarpinosPizza.Models.ViewModels;
+using DarpinosPizza.Models;
+using System.ComponentModel;
+
+namespace DarpinosPizza.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User { Id = 1, Name = "Victor", Email = "VictorOdarp@gmail.com" });
+            users.Add(new User { Id = 2, Name = "Carlos", Email = "CarlosMagno@gmail.com" });
+            return View(users);
+        }
+    }
+}
---
+++ DarpinosPizza/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DarpinosPizza.Models.ViewModels;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
